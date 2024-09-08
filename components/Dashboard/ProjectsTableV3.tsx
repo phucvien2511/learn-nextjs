a6449fcfac51d19fa15fa8ab86e85b472412aa10
minhphucvien <vienphuc2511@gmail.com>
2024-09-08 22:21:37 +0700

    customize header

--- components/Dashboard/ProjectsTableV3.tsx
+++ components/Dashboard/ProjectsTableV3.tsx
@@ -55,17 +55,35 @@ const ProjectsTableV3 = () => {
     const columns = [
         {
             key: "work_date",
-            label: "Project date",
+            label: (
+                <>
+                    <div>Project number</div>
+                    <hr></hr>
+                    <div>Project name</div>
+                </>
+            ),
         },
         ...testProjects.map((project) => ({
             key: project.project_number,
-            label: project.project_name,
+            label: (
+                <>
+                    <div>{project.project_number}</div>
+                    <hr></hr>
+                    <div>{project.project_name}</div>
+                </>
+            ),
         })),
     ];
 
     const createRowData = (workDate: WorkDate, projectNum: number) => {
         const rowData: { [key: string]: any } = {
-            work_date: workDate.date + " " + workDate.day,
+            // work_date: workDate.date + " " + workDate.day,
+            work_date: (
+                <div className="flex justify-between w-[144px]">
+                    <span>{workDate.date}</span>
+                    <span>{workDate.day}</span>
+                </div>
+            ),
         };
 
         for (let i = 1; i <= projectNum; i++) {
@@ -79,8 +97,13 @@ const ProjectsTableV3 = () => {
     const rows = workDates.map((item) =>
         createRowData(item, testProjects.length)
     );
+    // Customize styles for table
+    const classNames = {
+        base: ["w-full"],
+        tr: ["[&>th]:first:text-xl"], // Change header row style
+    };
     return (
-        <Table>
+        <Table isStriped isHeaderSticky classNames={classNames}>
             <TableHeader columns={columns}>
                 {(column) => (
                     <TableColumn key={column.key}>{column.label}</TableColumn>
